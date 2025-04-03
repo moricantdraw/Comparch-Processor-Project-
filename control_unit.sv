5e06dd50732ecd30c554c3da201468440de5eb5d
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-04-03 18:01:33 -0400

    Work on Control Unit

---
+++ control_unit.sv
@@ -0,0 +1,16 @@
+module control_unit (
+    input logic     clk,
+    input logic     [6:0] op,
+    input logic     [2:0] funct3,
+    input logic     funct7,
+    output logic    PCSrc,
+    output logic    [1:0]ResultSrc,
+    output logic    MemWrite,
+    output logic    [2:0]ALUControl,
+    output logic    ALUSrc,
+    output logic    [1:0]ImmSrc,
+    output logic    RegWrite
+);
+    logic 
+
+endmodule
