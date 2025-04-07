e8a68d54d81d053136d56e4bf55f704d5c906d74
yanashley <ashley.of.yang@gmail.com>
2025-04-07 00:10:09 -0400

    finish ALU and SrcB mux

--- ALU.sv
+++ ALU.sv
@@ -1,7 +1,7 @@
 module ALU (
     input logic clk,
-    input logic SrcA,
-    input logic SrcB,
+    input logic [31:0] SrcA,
+    input logic [31:0] SrcB,
     input logic [2:0] ALUControl,
     output logic Zero,
     output logic [31:0] ALUResult
@@ -10,15 +10,34 @@ module ALU (
 // depending on 
 always_ff @(psedge clk) begin
     case (ALUControl)
-        3'b000:
-            ALUResult = SrcA + SrcB;
-        3'b001:
-            ALUResult = SrcA - SrcBl
-        3'b101: // set less than
-        3'b011: // or
-        3'010: //  
+        3'b000: ALUResult = SrcA + SrcB;
+        3'b001: ALUResult = SrcA - SrcBl
+        3'b011: ALUResult = SrcA | SrcB; // or
+        3'b010: ALUResult = SrcA & SrcB; // and
+        3'b101: ALUResult = (SrcA + SrcB)[31]; // slt (set less than) -- are we using two's complement?
+        default: ALUResult = 32'bx;
     endcase
 end
-
-
 endmodule
+
+
+// textbook exercise 5.13 solution (ALU expanded to support SLT)
+// module alu(input  logic [31:0] a,
+//            input  logic [31:0] b,
+//            input  logic [2:0]  alucontrol,
+//            output logic [31:0] result);
+
+// logic [31:0] condinvb, sum;
+// logic        cout;           // carry out of adder
+// assign condinvb = alucontrol[0] ? ~b : b;
+// assign {cout, sum} = a + condinvb + alucontrol[0];
+//   always_comb
+//     case (alucontrol)
+//       3'b000:   result = sum;
+//       3'b001:   result = sum;
+//       3'b010:   result = a & b;
+//       3'b011:   result = a | b;
+//       3'b101:   result = sum[31];
+//       default:  result = 32'bx;
+//     endcase
+// endmodule
