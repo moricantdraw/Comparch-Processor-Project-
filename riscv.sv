06aa7398d17ee54a88ea8d15386bf1263f09fc40
moricantdraw <mchevere@olin.edu>
2025-04-10 11:54:54 -0400

    modifying

--- riscv.sv
+++ riscv.sv
@@ -48,34 +48,33 @@ module riscv(input logic clk, reset);
 					  input logic [2:0] ImmSrc, 
 					  input logic [3:0] ALUControl, 
 					  input logic [1:0] ResultSrc, 
-					  input logic IRWrite,
 					  input logic RegWrite,
 					  input logic [1:0] ALUSrcA, ALUSrcB, 
 					  input logic AdrSrc, 
 					  input logic PCWrite,  
 					  input logic [31:0] ReadData,
-					  output logic Zero, cout, overflow, sign, 
-					  output logic [31:0] Adr, 
-					  output logic [31:0] WriteData,
-					  output logic [31:0] instr);
+					  output logic [31:0] ALUResult, WriteData,
+					  input logic [31:0] instr;
+                      output logic [31:0] PC );
+                      output logic []
 
 		 
-logic [31:0] Result , ALUOut, ALUResult;
-logic [31:0] RD1, RD2, A , SrcA, SrcB, Data;
+logic [31:0] Result;
+logic [31:0] SrcA, SrcB;
 logic [31:0] ImmExt;
-logic [31:0] PC, OldPC;
+logic [31:0] PCNext, PCPlus4, PCTarget;
 
 
 //pc
+flopr#(32) pcreg(clk, reset, PCNext, PC);
+adder   pcadd4(PC, 32'd4, PCPlus4);
+adder   pcaddbranch (PC, ImmExt, PCTarget)
 flopenr #(32) pcFlop(clk, reset, PCWrite, Result, PC);
 
 
 //regFile
-regFile rf(clk, RegWrite, instr[19:15], instr[24:20], instr[11:7], Result, RD1, RD2); 
+regFile rf(clk, RegWrite, instr[19:15], instr[24:20], instr[11:7], Result, SrcA, WriteData); 
 extend ext(instr[31:7], ImmSrc, ImmExt);
-flopr #(32) regF( clk, reset, RD1, A);
-flopr #(32) regF_2( clk, reset, RD2, WriteData);
-
 
 //alu
 mux3 #(32) srcAmux(PC, OldPC, A, ALUSrcA, SrcA);
@@ -91,14 +90,4 @@ flopenr #(32) memFlop2(clk, reset, IRWrite, ReadData, instr);
 flopr #(32) memDataFlop(clk, reset, ReadData, Data);
 
 
-/* do we need anything for the memory 
-    // === Memory (both instruction + data) ===
-    memory mem(
-        .clk(clk),
-        .we(MemWrite),
-        .a(DataAdr),
-        .wd(WriteData),
-        .rd(ReadData)
-    );
-*/
 endmodule
