c1a77bd8cb234655cc381b0dc771fbfd87df24b5
yanashley <ashley.of.yang@gmail.com>
2025-04-17 11:44:27 -0400

    implement nonarchitectural register Instr

---
+++ register_Instr.sv
@@ -0,0 +1,19 @@
+module register_Instr(
+    input logic [31:0] RD,
+    input logic [31:0] PC,
+    input logic IRWrite,
+    output logic [31:0] Instr,
+    output logic [31:0] OldPC
+);
+
+    always_ff@(posedge clk) begin
+        // when IRWrite asserted from Control Unit during fetch, Instr stored
+        if (IRWrite) begin
+            Instr <= RD;
+
+            // OldPC syncs with being captured at the same time too
+            OldPC <= PC;
+        end
+    end
+
+endmodule
