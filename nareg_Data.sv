a46fb759408b40053c3afbac1dcc624cbdbece95
yanashley <ashley.of.yang@gmail.com>
2025-04-23 03:14:27 -0400

    fix Data nareg to not be behind

--- nareg_Data.sv
+++ nareg_Data.sv
@@ -4,12 +4,13 @@ module nareg_Data(
     input logic     [31:0] ReadData,
     output logic    [31:0] Data
 );
-    always_ff@(posedge clk, posedge rst) begin
-        if (rst) begin
-            Data <= 32'h00000000; // Reset Data to 0
-        end else begin
-            // when Data is updated, store the ReadData
-            Data <= ReadData;
-        end
-    end
+    assign Data = ReadData; // Directly assign ReadData to Data
+    // always_ff@(posedge clk, posedge rst) begin
+    //     if (rst) begin
+    //         Data <= 32'h00000000; // Reset Data to 0
+    //     end else begin
+    //         // when Data is updated, store the ReadData
+    //         Data <= ReadData;
+    //     end
+    // end
 endmodule
